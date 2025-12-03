abbe4a1ccf5271c785c6fd41d013ac278ceb0e83
Vladislav <vmitk@mail.ru>
2025-12-03 21:30:03 +0300

    pause and time freeze

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ from google import genai
 import wave
 import sys
 import quetions
+from pynput.keyboard import Key, Controller
+import time
 
 def resource_path(relative_path):
     """Функция для получения абсолютного пути к файлам (учитывает как режим разработки, так и работу через PyInstaller)"""
@@ -48,23 +50,44 @@ def command(text: str) -> bool:
                     if action.startswith("sp/run"): # Если sp/run - запуск команды в терминале
                         command = action.replace("sp/run ", "")
                         subprocess.run(command, shell=True)
+                    if action.startswith("keyboard"): # Если keyboard.что-то, выполняется что-то с клавиатурой
+                        action = action.replace("keyboard.", "")
+                        keyboard = Controller()
+                        if action.startswith("press"):
+                            button = action.replace("press ", "")
+                            if len(button) == 1:
+                                keyboard.press(button)
+                            else:
+                                keyboard.press(Key[button])
+                        if action.startswith("release"):
+                            button = action.replace("release ", "")
+                            if len(button) == 1:
+                                keyboard.release(button)
+                            else:
+                                keyboard.release(Key[button])
+                        if action.startswith("tap"):
+                            button = action.replace("tap ", "")
+                            if len(button) == 1:
+                                keyboard.tap(button)
+                            else:
+                                keyboard.tap(Key[button])
+                        if action.startswith("type"):
+                            text = action.replace("type ", "")
+                            keyboard.type(text)
+                    if action.startswith("freeze"): # Если freeze, задержка на столько секунд, сколько надо
+                        sleep_time = action.replace("freeze ", "")
+                        time.sleep(int(sleep_time))
                     if action == "cancel": # Если команда отмены, возвращаем False
                         return False
 
-    if not command_was_executed: 
-        response = client.models.generate_content( # Иначе генерируем ответ с помощью genai
-            model="gemini-2.0-flash", 
-            contents=f'{
-                quetions.language.join(
-                text.join(
-                load_yaml_file(resource_path("config.yaml"))["ai-data"]
-                .split("{text}"))
-                .split("{language}"))
-                }'  
-                # Заменяем {text} на текст, сказанный пользователем и {language} на выбранный язык
-        )
-        print(response.text)
-        text_to_speech(response.text)
+    # if not command_was_executed: 
+    #     response = client.models.generate_content( # Иначе генерируем ответ с помощью genai
+    #         model="gemini-2.0-flash", 
+    #         contents=f'{quetions.language.join(text.join(load_yaml_file(resource_path("config.yaml"))["ai-data"].split("{text}")).split("{language}"))}'  
+    #             # Заменяем {text} на текст, сказанный пользователем и {language} на выбранный язык
+    #     )
+    #     print(response.text)
+    #     text_to_speech(response.text)
     return True
 
 def text_to_speech(text: str) -> None:
